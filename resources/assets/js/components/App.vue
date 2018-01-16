2bc2000234d9645ca27457904ca6a7e7509565a8
HeinHtetAung <heinhtetaung.sglife@gmail.com>
2018-01-16 14:54:14 +0630

    vue start up

---
+++ resources/assets/js/components/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <nav>
+                <ul class="list-inline">
+                    <li>
+                        <router-link :to="{ name: 'example' }">Example</router-link>
+                    </li>
+                    
+                </ul>
+            </nav>
+        </div>
+        <div class="panel-body">
+            <transition name="fade">
+                <router-view></router-view>
+            </transition>
+        </div>
+    </div>
+
+</template>
+<style>
+    .fade-enter-active, .fade-leave-active {
+      transition: opacity .5s
+    }
+    .fade-enter, .fade-leave-active {
+      opacity: 0
+    }
+</style>
+<script>
+    export default{
+    }
+</script>
